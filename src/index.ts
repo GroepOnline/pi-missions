d57eec68ed60b0cdb045970511ac11ffea0e3e98
OnlineChef <chefadmin@chefgroep.online>
2026-08-22 08:26:04 +0200

    feat(missions): export Orchestra correlation helpers (CHE-142)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 export { default } from "./core/extension.js";
+export {
+  buildMissionOrchestraExecutionCorrelation,
+  buildOrchestraExecutionIdempotencyKey,
+  ORCHESTRA_EXECUTION_CONTRACT_VERSION,
+  orchestraCorrelationEnv,
+} from "./integrations/orchestra-execution.js";
+export type {
+  MissionOrchestraExecutionCorrelation,
+  MissionOrchestraExecutionIdentity,
+} from "./integrations/orchestra-execution.js";
